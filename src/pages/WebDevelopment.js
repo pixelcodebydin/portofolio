c843276a46acefa9870155f6f781cff536956227
pixelcodebydin <official.dindajuliap@gmail.com>
2024-12-18 19:15:55 +0700

    web development done

--- src/pages/WebDevelopment.js
+++ src/pages/WebDevelopment.js
@@ -19,7 +19,7 @@ function WebDevelopment() {
                 <a href="https://github.com/pixelcodebydin/" target="_blank"><Button /></a>
             </div>
 
-            <hr id="line" className="mb-2"></hr>
+            <hr id="line" className="mb-3"></hr>
 
             <div className="row pt-4 pb-5">
                 <div className="col-xl-5 col-lg-5 col-md-12 col-sm-12 col-xs-12 mt-4" id="desc">
@@ -32,15 +32,15 @@ function WebDevelopment() {
                     <div id="carousel-1" className="carousel slide" data-bs-ride="carousel">
                         <div className="carousel-inner">
                             <div className="carousel-item active">
-                                <img src="/image/web-development/Web - 1.jpg" className="d-block" alt="Slide 1" id="slide" />
+                                <img src="/image/web-development/Web - 1.png" className="d-block" alt="Slide 1" id="slide" />
                             </div>
 
                             <div className="carousel-item">
-                                <img src="/image/web-development/Web - 2.jpg" className="d-block" alt="Slide 2" id="slide" />
+                                <img src="/image/web-development/Web - 2.png" className="d-block" alt="Slide 2" id="slide" />
                             </div>
 
                             <div className="carousel-item">
-                                <img src="/image/web-development/Web - 3.jpg" className="d-block" alt="Slide 3" id="slide" />
+                                <img src="/image/web-development/Web - 3.png" className="d-block" alt="Slide 3" id="slide" />
                             </div>
                         </div>
 
@@ -50,6 +50,159 @@ function WebDevelopment() {
                         </button>
                         
                         <button className="carousel-control-next" type="button" data-bs-target="#carousel-1" data-bs-slide="next">
+                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span className="visually-hidden">Next</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            <hr id="line" className="mb-2"></hr>
+
+            <div className="row pt-4 pb-5">
+                <div className="col-xl-5 col-lg-5 col-md-12 col-sm-12 col-xs-12 mt-4" id="desc">
+                    <h3>CV. Tamanta Indo Ekspor</h3>
+                    <p>Building and designing the personal website of CV. Tamanta Indo Ekspor using PHP, MySQL, and CodeIgniter 3, that shows information about the company and services that offered by CV. Tamanta Indo Ekspor.</p>
+                    <a href="https://tamantaindoekspor.com/id/index.php" target="_blank"><Button /></a>
+                </div>
+
+                <div className="col-xl-7 col-lg-7 col-md-12 col-sm-12 col-xs-12 mt-4">
+                    <div id="carousel-2" className="carousel slide" data-bs-ride="carousel">
+                        <div className="carousel-inner">
+                            <div className="carousel-item active">
+                                <img src="/image/web-development/Web - 4.png" className="d-block" alt="Slide 1" id="slide" />
+                            </div>
+
+                            <div className="carousel-item">
+                                <img src="/image/web-development/Web - 5.png" className="d-block" alt="Slide 2" id="slide" />
+                            </div>
+
+                            <div className="carousel-item">
+                                <img src="/image/web-development/Web - 6.png" className="d-block" alt="Slide 3" id="slide" />
+                            </div>
+                        </div>
+
+                        <button className="carousel-control-prev" type="button" data-bs-target="#carousel-2" data-bs-slide="prev">
+                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span className="visually-hidden">Previous</span>
+                        </button>
+                        
+                        <button className="carousel-control-next" type="button" data-bs-target="#carousel-2" data-bs-slide="next">
+                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span className="visually-hidden">Next</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            <hr id="line" className="mb-2"></hr>
+
+            <div className="row pt-4 pb-5">
+                <div className="col-xl-5 col-lg-5 col-md-12 col-sm-12 col-xs-12 mt-4" id="desc">
+                    <h3>Dashboard of PED Division, Telkom Regional 1</h3>
+                    <p>Modifying the dashboard of PED division, Telkom Regional 1, which contains information about witel data, cb, zfm and zrmm, etc.</p>
+                </div>
+
+                <div className="col-xl-7 col-lg-7 col-md-12 col-sm-12 col-xs-12 mt-4">
+                    <div id="carousel-3" className="carousel slide" data-bs-ride="carousel">
+                        <div className="carousel-inner">
+                            <div className="carousel-item active">
+                                <img src="/image/web-development/Web - 7.png" className="d-block" alt="Slide 1" id="slide" />
+                            </div>
+
+                            <div className="carousel-item">
+                                <img src="/image/web-development/Web - 8.png" className="d-block" alt="Slide 2" id="slide" />
+                            </div>
+
+                            <div className="carousel-item">
+                                <img src="/image/web-development/Web - 9.png" className="d-block" alt="Slide 3" id="slide" />
+                            </div>
+                        </div>
+
+                        <button className="carousel-control-prev" type="button" data-bs-target="#carousel-3" data-bs-slide="prev">
+                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span className="visually-hidden">Previous</span>
+                        </button>
+                        
+                        <button className="carousel-control-next" type="button" data-bs-target="#carousel-3" data-bs-slide="next">
+                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span className="visually-hidden">Next</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            <hr id="line" className="mb-2"></hr>
+
+            <div className="row pt-4 pb-5">
+                <div className="col-xl-5 col-lg-5 col-md-12 col-sm-12 col-xs-12 mt-4" id="desc">
+                    <h3>Konflix</h3>
+                    <p>Duplicating the Netflix webstie using PHP, MySQL, and CodeIgniter 3 to fulfill the assignment for Database Systems subject.</p>
+                </div>
+
+                <div className="col-xl-7 col-lg-7 col-md-12 col-sm-12 col-xs-12 mt-4">
+                    <div id="carousel-4" className="carousel slide" data-bs-ride="carousel">
+                        <div className="carousel-inner">
+                            <div className="carousel-item active">
+                                <img src="/image/web-development/Web - 10.png" className="d-block" alt="Slide 1" id="slide" />
+                            </div>
+
+                            <div className="carousel-item">
+                                <img src="/image/web-development/Web - 11.png" className="d-block" alt="Slide 2" id="slide" />
+                            </div>
+
+                            <div className="carousel-item">
+                                <img src="/image/web-development/Web - 12.png" className="d-block" alt="Slide 3" id="slide" />
+                            </div>
+                        </div>
+
+                        <button className="carousel-control-prev" type="button" data-bs-target="#carousel-4" data-bs-slide="prev">
+                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span className="visually-hidden">Previous</span>
+                        </button>
+                        
+                        <button className="carousel-control-next" type="button" data-bs-target="#carousel-4" data-bs-slide="next">
+                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span className="visually-hidden">Next</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            <hr id="line" className="mb-2"></hr>
+
+            <div className="row pt-4 pb-5">
+                <div className="col-xl-5 col-lg-5 col-md-12 col-sm-12 col-xs-12 mt-4" id="desc">
+                    <h3>Almanak HKBP</h3>
+                    <p>Building a dashboard to monitor HKBP services that presents information about districts, resorts, huria, and the servants. The dashboard also provides information about almanac that includes reflections.</p>
+                </div>
+
+                <div className="col-xl-7 col-lg-7 col-md-12 col-sm-12 col-xs-12 mt-4">
+                    <div id="carousel-5" className="carousel slide" data-bs-ride="carousel">
+                        <div className="carousel-inner">
+                            <div className="carousel-item active">
+                                <img src="/image/web-development/Web - 13.png" className="d-block" alt="Slide 1" id="slide" />
+                            </div>
+
+                            <div className="carousel-item">
+                                <img src="/image/web-development/Web - 14.png" className="d-block" alt="Slide 2" id="slide" />
+                            </div>
+
+                            <div className="carousel-item">
+                                <img src="/image/web-development/Web - 15.png" className="d-block" alt="Slide 3" id="slide" />
+                            </div>
+
+                            <div className="carousel-item">
+                                <img src="/image/web-development/Web - 16.png" className="d-block" alt="Slide 4" id="slide" />
+                            </div>
+                        </div>
+
+                        <button className="carousel-control-prev" type="button" data-bs-target="#carousel-5" data-bs-slide="prev">
+                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span className="visually-hidden">Previous</span>
+                        </button>
+                        
+                        <button className="carousel-control-next" type="button" data-bs-target="#carousel-5" data-bs-slide="next">
                             <span className="carousel-control-next-icon" aria-hidden="true"></span>
                             <span className="visually-hidden">Next</span>
                         </button>
